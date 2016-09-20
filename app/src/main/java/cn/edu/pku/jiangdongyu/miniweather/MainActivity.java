52eb37bc5fddf8be697a42f3cc8b23ed553817a7
12301068JiangDongyu <12301068@bjtu.edu.cn>
2016-09-20 21:17:46 +0800

    init project

---
+++ app/src/main/java/cn/edu/pku/jiangdongyu/miniweather/MainActivity.java
@@ -0,0 +1,16 @@
+package cn.edu.pku.jiangdongyu.miniweather;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by jiangdongyu on 16/9/20.
+ */
+public class MainActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+
+    }
+}
