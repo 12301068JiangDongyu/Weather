0d050d858471a05c90603b0c145b7dd7a143a160
12301068JiangDongyu <12301068@bjtu.edu.cn>
2016-12-06 23:06:46 +0800

    实现定位功能
    百度地图API
    根据返回的getDistrict进行更新

--- app/src/main/java/cn/edu/pku/jiangdongyu/miniweather/MainActivity.java
+++ app/src/main/java/cn/edu/pku/jiangdongyu/miniweather/MainActivity.java
@@ -24,7 +24,14 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.location.Poi;
+
 import cn.edu.pku.jiangdongyu.adapter.ForcastFragmentPageAdapter;
+import cn.edu.pku.jiangdongyu.app.MyApplication;
 import cn.edu.pku.jiangdongyu.fragment.MyFragment;
 import cn.edu.pku.jiangdongyu.service.UpdateWeatherService;
 import cn.edu.pku.jiangdongyu.bean.FutureWeather;
@@ -38,10 +45,15 @@ import cn.edu.pku.jiangdongyu.util.ParseData;
 public class MainActivity extends FragmentActivity implements View.OnClickListener{
     private static final int UPDATE_TODAY_WEATHER = 1;
     private static final int UPDATE_FUTURE_WEATHER = 2;
-    private ImageView mUpdateBtn;
-    private ImageView mCitySelect;
+
+    public LocationClient mLocationClient = null;
+    public BDLocationListener myListener = new MyLocationListener();
+
+    private static Handler mHandler = null;
+
+    private ImageView mUpdateBtn,mCitySelect,mLocation;
     private ProgressBar mTitleUpdateProgress;
-    private TextView cityTv,timeTv,humidityTv,weekTv,pmDataTv,pmQualityTv,temperatureTv,climateTv,windTv,city_name_Tv,temp_now_Tv,textView;
+    private TextView cityTv,timeTv,humidityTv,weekTv,pmDataTv,pmQualityTv,temperatureTv,climateTv,windTv,city_name_Tv,temp_now_Tv;
     private ImageView weatherImg,pmImg;
 
     private MyWeatherBroadcast mWeatherBroadcast;
@@ -53,22 +65,29 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
 
     private ImageView[] dots;
     private int[] ids = {R.id.iv4,R.id.iv5};
+
+    public static Handler getHandler(){
+        return mHandler;
+    }
+
     //更新后的cityCode
     private String newCityCode = "101010100";
-    private Handler mHandler = new Handler(){
-        public void handleMessage(android.os.Message msg){
-            switch (msg.what){
-                case UPDATE_TODAY_WEATHER:
-                    updateTodayWeather((TodayWeather) msg.obj);
-                    break;
-                case UPDATE_FUTURE_WEATHER:
-                    updateFutureWeather((List<FutureWeather>) msg.obj);
-                    break;
-                default:
-                    break;
+    public void initHandler(){
+        mHandler = new Handler(){
+            public void handleMessage(android.os.Message msg){
+                switch (msg.what){
+                    case UPDATE_TODAY_WEATHER:
+                        updateTodayWeather((TodayWeather) msg.obj);
+                        break;
+                    case UPDATE_FUTURE_WEATHER:
+                        updateFutureWeather((List<FutureWeather>) msg.obj);
+                        break;
+                    default:
+                        break;
+                }
             }
-        }
-    };
+        };
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -78,7 +97,11 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
 
         mTitleUpdateProgress = (ProgressBar) findViewById(R.id.title_update_progress);
         mUpdateBtn = (ImageView) findViewById(R.id.title_update_btn);
+        mLocation = (ImageView) findViewById(R.id.title_location);
+        mCitySelect = (ImageView) findViewById(R.id.title_city_manager);
+        mCitySelect.setOnClickListener(this);
         mUpdateBtn.setOnClickListener(this);
+        mLocation.setOnClickListener(this);
 
         if(NetUtil.getNetworkState(this) != NetUtil.NETWORN_NONE) {
             Log.d("myWeather","网络OK");
@@ -88,10 +111,14 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
             Toast.makeText(MainActivity.this,"网络挂了！",Toast.LENGTH_LONG).show();
         }
 
-        mCitySelect = (ImageView) findViewById(R.id.title_city_manager);
-        mCitySelect.setOnClickListener(this);
+
         initView();
         initAfterView(null);
+        initHandler();
+
+        mLocationClient = new LocationClient(getApplicationContext());     //声明LocationClient类
+        mLocationClient.registerLocationListener( myListener );    //注册监听函数
+        initLocation();
 
         startWeatherService();
     }
@@ -135,8 +162,6 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
     }
 
 
-
-
     public class MyWeatherBroadcast extends BroadcastReceiver {
 
         @Override
@@ -147,6 +172,24 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
             queryWeatherCode(cityCode);
 
         }
+    }
+
+    private void initLocation(){
+        LocationClientOption option = new LocationClientOption();
+        option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy
+        );//可选，默认高精度，设置定位模式，高精度，低功耗，仅设备
+        option.setCoorType("bd09ll");//可选，默认gcj02，设置返回的定位结果坐标系
+        int span=1000;
+        option.setScanSpan(span);//可选，默认0，即仅定位一次，设置发起定位请求的间隔需要大于等于1000ms才是有效的
+        option.setIsNeedAddress(true);//可选，设置是否需要地址信息，默认不需要
+        option.setOpenGps(true);//可选，默认false,设置是否使用gps
+        option.setLocationNotify(true);//可选，默认false，设置是否当GPS有效时按照1S/1次频率输出GPS结果
+        option.setIsNeedLocationDescribe(true);//可选，默认false，设置是否需要位置语义化结果，可以在BDLocation.getLocationDescribe里得到，结果类似于“在北京天安门附近”
+        option.setIsNeedLocationPoiList(true);//可选，默认false，设置是否需要POI结果，可以在BDLocation.getPoiList里得到
+        option.setIgnoreKillProcess(false);//可选，默认true，定位SDK内部是一个SERVICE，并放到了独立进程，设置是否在stop的时候杀死这个进程，默认不杀死
+        option.SetIgnoreCacheException(false);//可选，默认false，设置是否收集CRASH信息，默认收集
+        option.setEnableSimulateGps(false);//可选，默认false，设置是否需要过滤GPS仿真结果，默认需要
+        mLocationClient.setLocOption(option);
     }
 
     //初始化页面控件
@@ -455,6 +498,11 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
                 Toast.makeText(MainActivity.this,"网络挂了！",Toast.LENGTH_LONG).show();
             }
         }
+
+        if(v.getId() == R.id.title_location){
+            mLocationClient.start();
+
+        }
     }
     //接收返回的数据
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -470,6 +518,77 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
                 Log.d("myWeather","网络挂了");
                 Toast.makeText(MainActivity.this,"网络挂了！",Toast.LENGTH_LONG).show();
             }
+        }
+    }
+
+    public class MyLocationListener implements BDLocationListener {
+
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+            //Receive Location
+            StringBuffer sb = new StringBuffer(256);
+            sb.append("time : ");
+            sb.append(location.getTime());
+            sb.append("\nerror code : ");
+            sb.append(location.getLocType());
+            sb.append("\nlatitude : ");
+            sb.append(location.getLatitude());
+            sb.append("\nlontitude : ");
+            sb.append(location.getLongitude());
+            sb.append("\nradius : ");
+            sb.append(location.getRadius());
+            if (location.getLocType() == BDLocation.TypeGpsLocation) {// GPS定位结果
+                sb.append("\nspeed : ");
+                sb.append(location.getSpeed());// 单位：公里每小时
+                sb.append("\nsatellite : ");
+                sb.append(location.getSatelliteNumber());
+                sb.append("\nheight : ");
+                sb.append(location.getAltitude());// 单位：米
+                sb.append("\ndirection : ");
+                sb.append(location.getDirection());// 单位度
+                sb.append("\naddr : ");
+                sb.append(location.getAddrStr());
+                sb.append("\ndescribe : ");
+                sb.append("gps定位成功");
+
+            } else if (location.getLocType() == BDLocation.TypeNetWorkLocation) {// 网络定位结果
+                sb.append("\naddr : ");
+                sb.append(location.getAddrStr());
+                //运营商信息
+                sb.append("\noperationers : ");
+                sb.append(location.getOperators());
+                sb.append("\ndescribe : ");
+                sb.append("网络定位成功");
+            } else if (location.getLocType() == BDLocation.TypeOffLineLocation) {// 离线定位结果
+                sb.append("\ndescribe : ");
+                sb.append("离线定位成功，离线定位结果也是有效的");
+            } else if (location.getLocType() == BDLocation.TypeServerError) {
+                sb.append("\ndescribe : ");
+                sb.append("服务端网络定位失败，可以反馈IMEI号和大体定位时间到loc-bugs@baidu.com，会有人追查原因");
+            } else if (location.getLocType() == BDLocation.TypeNetWorkException) {
+                sb.append("\ndescribe : ");
+                sb.append("网络不同导致定位失败，请检查网络是否通畅");
+            } else if (location.getLocType() == BDLocation.TypeCriteriaException) {
+                sb.append("\ndescribe : ");
+                sb.append("无法获取有效定位依据导致定位失败，一般是由于手机的原因，处于飞行模式下一般会造成这种结果，可以试着重启手机");
+            }
+            sb.append("\nlocationdescribe : ");
+            sb.append(location.getLocationDescribe());// 位置语义化信息
+            List<Poi> list = location.getPoiList();// POI数据
+            if (list != null) {
+                sb.append("\npoilist size = : ");
+                sb.append(list.size());
+                for (Poi p : list) {
+                    sb.append("\npoi= : ");
+                    sb.append(p.getId() + " " + p.getName() + " " + p.getRank());
+                }
+            }
+            Log.i("BaiduLocationApiDem", sb.toString());
+            String cityName = location.getDistrict();
+            String code = MyApplication.getInstance().getCityCodeByName(cityName);
+            Toast.makeText(MainActivity.this,"城市："+cityName,Toast.LENGTH_LONG).show();
+            queryWeatherCode(code);
+            mLocationClient.stop();
         }
     }
 }
